49fe433db8d297db1a6e7371346a77a516ac85d4
d3code-dev <dev@d3code.dev>
2024-04-02 18:46:38 -0400

    TODO: refactor using string.prototype.repeat()

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,14 @@
-const repeatString = function() {
-
-};
+const repeatString = function(word, number) {   
+    let repeatedWord = '';
+    if (number >= 0) {
+    for (let i = 0; number > i; i++) {
+        repeatedWord += word;
+    }
+    return repeatedWord;
+    } else {
+        return 'ERROR';
+    }
+}
 
 // Do not edit below this line
 module.exports = repeatString;
